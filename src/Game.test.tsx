f1713b28da5b15dde656b1461c4e809ee3c781bf
Rick Haws <richard.haws@us.af.mil>
2024-03-25 17:06:30 -0600

    Started putting empty spaces around rings

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -1,13 +1,19 @@
 import * as Game from './Game';
 
-    //	\0\1\2\3\4\5\6\
-    // 0 \ \ \ \O\O\O\O\
-	// 1  \ \ \O\O\O\O\O\
-	// 2   \ \O\O\O\O\O\O\
-	// 3    \O\O\O\O\O\O\O\
-	// 4     \O\O\O\O\O\O\ \
-	// 5      \O\O\O\O\O\ \ \
-	// 6       \O\O\O\O\ \ \ \
+// Representation of Zertz board.
+// Note that rows 0 & 8 and columns 0 & 8
+//    are empty so that all cells in 1-7
+//    have a full complement of neighbors
+    //  \0\1\2\3\4\5\6\7\8\
+    // 0 \ \ \ \ \ \ \ \ \ \
+    // 1  \ \ \ \ \O\O\O\O\ \
+    // 2   \ \ \ \O\O\O\O\O\ \
+    // 3    \ \ \O\O\O\O\O\O\ \
+    // 4     \ \O\O\O\O\O\O\O\ \
+    // 5      \ \O\O\O\O\O\O\ \ \
+    // 6       \ \O\O\O\O\O\ \ \ \
+    // 7        \ \O\O\O\O\ \ \ \ \
+    // 8         \ \ \ \ \ \ \ \ \ \
 
 describe('Game state tests', () => {
     it('Tests initialize', () => {
@@ -20,28 +26,28 @@ describe('Game state tests', () => {
         expect(Game.getState(0, 0)).toBe('Removed');
         expect(Game.getState(0, 1)).toBe('Removed');
         expect(Game.getState(0, 2)).toBe('Removed');
-        expect(Game.getState(0, 3)).toBe('Open');
-        expect(Game.getState(0, 4)).toBe('Open');
-        expect(Game.getState(0, 5)).toBe('Open');
-        expect(Game.getState(0, 6)).toBe('Open');
+        expect(Game.getState(0, 3)).toBe('Removed');
+        expect(Game.getState(0, 4)).toBe('Removed');
+        expect(Game.getState(0, 5)).toBe('Removed');
+        expect(Game.getState(0, 6)).toBe('Removed');
 
         expect(Game.getState(1, 0)).toBe('Removed');
         expect(Game.getState(1, 1)).toBe('Removed');
-        expect(Game.getState(1, 2)).toBe('Open');
+        expect(Game.getState(1, 2)).toBe('Removed');
         expect(Game.getState(1, 3)).toBe('Open');
         expect(Game.getState(1, 4)).toBe('Open');
         expect(Game.getState(1, 5)).toBe('Open');
         expect(Game.getState(1, 6)).toBe('Open');
 
         expect(Game.getState(2, 0)).toBe('Removed');
-        expect(Game.getState(2, 1)).toBe('Open');
+        expect(Game.getState(2, 1)).toBe('Removed');
         expect(Game.getState(2, 2)).toBe('Open');
         expect(Game.getState(2, 3)).toBe('Open');
         expect(Game.getState(2, 4)).toBe('Open');
         expect(Game.getState(2, 5)).toBe('Open');
         expect(Game.getState(2, 6)).toBe('Open');
 
-        expect(Game.getState(3, 0)).toBe('Open');
+        expect(Game.getState(3, 0)).toBe('Removed');
         expect(Game.getState(3, 1)).toBe('Open');
         expect(Game.getState(3, 2)).toBe('Open');
         expect(Game.getState(3, 3)).toBe('Open');
@@ -55,22 +61,42 @@ describe('Game state tests', () => {
         expect(Game.getState(4, 3)).toBe('Open');
         expect(Game.getState(4, 4)).toBe('Open');
         expect(Game.getState(4, 5)).toBe('Open');
-        expect(Game.getState(4, 6)).toBe('Removed');
+        expect(Game.getState(4, 6)).toBe('Open');
 
         expect(Game.getState(5, 0)).toBe('Open');
         expect(Game.getState(5, 1)).toBe('Open');
         expect(Game.getState(5, 2)).toBe('Open');
         expect(Game.getState(5, 3)).toBe('Open');
         expect(Game.getState(5, 4)).toBe('Open');
-        expect(Game.getState(5, 5)).toBe('Removed');
+        expect(Game.getState(5, 5)).toBe('Open');
         expect(Game.getState(5, 6)).toBe('Removed');
 
         expect(Game.getState(6, 0)).toBe('Open');
         expect(Game.getState(6, 1)).toBe('Open');
         expect(Game.getState(6, 2)).toBe('Open');
         expect(Game.getState(6, 3)).toBe('Open');
-        expect(Game.getState(6, 4)).toBe('Removed');
+        expect(Game.getState(6, 4)).toBe('Open');
         expect(Game.getState(6, 5)).toBe('Removed');
         expect(Game.getState(6, 6)).toBe('Removed');
+
+        expect(Game.getState(7, 0)).toBe('Open');
+        expect(Game.getState(7, 1)).toBe('Open');
+        expect(Game.getState(7, 2)).toBe('Open');
+        expect(Game.getState(7, 3)).toBe('Open');
+        expect(Game.getState(7, 4)).toBe('Removed');
+        expect(Game.getState(7, 5)).toBe('Removed');
+        expect(Game.getState(7, 6)).toBe('Removed');
+
+        expect(Game.getState(8, 0)).toBe('Removed');
+        expect(Game.getState(8, 1)).toBe('Removed');
+        expect(Game.getState(8, 2)).toBe('Removed');
+        expect(Game.getState(8, 3)).toBe('Removed');
+        expect(Game.getState(8, 4)).toBe('Removed');
+        expect(Game.getState(8, 5)).toBe('Removed');
+        expect(Game.getState(8, 6)).toBe('Removed');
+    });
+    it('Tests getNeighbors', () => {
+        Game.initBoard();
+        expect(Game.getNeighbors(0, 0)).toHaveLength(2);
     });
 });
--- src/Game.tsx
+++ src/Game.tsx
@@ -2,13 +2,33 @@ export type SpaceState = 'Removed' | 'Open' | 'White' | 'Gray' | 'Black';
 
 export const BOARD_SIZE = 7;
 
-const board: SpaceState[][] = Array(BOARD_SIZE).fill(0).map(() => Array(BOARD_SIZE).fill('Open'));
+const board: SpaceState[][] = Array(BOARD_SIZE + 2).fill(0).map(() => Array(BOARD_SIZE + 2).fill('Open'));
+
+// Representation of Zertz board.
+// Note that rows 0 & 8 and columns 0 & 8
+//    are empty so that all cells in 1-7
+//    have a full complement of neighbors
+    //  \0\1\2\3\4\5\6\7\8\
+    // 0 \ \ \ \ \ \ \ \ \ \
+    // 1  \ \ \ \ \O\O\O\O\ \
+    // 2   \ \ \ \O\O\O\O\O\ \
+    // 3    \ \ \O\O\O\O\O\O\ \
+    // 4     \ \O\O\O\O\O\O\O\ \
+    // 5      \ \O\O\O\O\O\O\ \ \
+    // 6       \ \O\O\O\O\O\ \ \ \
+    // 7        \ \O\O\O\O\ \ \ \ \
+    // 8         \ \ \ \ \ \ \ \ \ \
 
 export const initBoard = () => {
-    for (let i=0; i < 3; i++) {
-        for (let j=0; j < 3-i; j++) {
-                        board[i][j] = 'Removed';
-                        board[BOARD_SIZE-i-1][BOARD_SIZE-j-1] = 'Removed';
+    for (let i = 0; i < BOARD_SIZE + 2; i++) {
+        board[0][i] = 'Removed';
+        board[i][0] = 'Removed';
+    }
+    
+    for (let i = 1; i < 4; i++) {
+        for (let j = 1; j < 4-i; j++) {
+            board[i][j] = 'Removed';
+            board[BOARD_SIZE-i+1][BOARD_SIZE-j+1] = 'Removed';
         }
     }
 }
@@ -19,14 +39,6 @@ export const canRemove = (row: number, col: number) =>
 {
     if (board[row][col] !== 'Open') return false;
 
-    //	\0\1\2\3\4\5\6\
-    // 0 \ \ \ \O\O\O\O\
-	// 1  \ \ \O\O\O\O\O\
-	// 2   \ \O\O\O\O\O\O\
-	// 3    \O\O\O\O\O\O\O\
-	// 4     \O\O\O\O\O\O\ \
-	// 5      \O\O\O\O\O\ \ \
-	// 6       \O\O\O\O\ \ \ \
 
     // List neighbor cells in circular fashion, repeating first at the end
     const neighbors = getNeighbors(row, col);
@@ -93,7 +105,7 @@ export const getJumps = () => {
     // then return a boolean mask of the board where stones with jumps available are
     // indicated as true and all else are shown as false.
 
-    let jumps: boolean[][] = Array(BOARD_SIZE).fill(0).map(row => Array(BOARD_SIZE).map(col => false));
+    let jumps: boolean[][] = Array(BOARD_SIZE + 2).fill(0).map(row => Array(BOARD_SIZE + 2).map(col => false));
     
     for (let i = 0; i < jumps.length; i++) {
         for (let j = 0; j < jumps[i].length; j++) {
